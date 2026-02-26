a4f556a9ee8b0a2a73421bf4ea63f908b98a4842
Derek Thuecks <derek.thuecks@gmail.com>
2026-02-26 01:34:43 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,59 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "front-colophon",
+  "level": "1",
+  "url": "front-colophon.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  "
+},
+{
+  "id": "sec-pizza-giordanos",
+  "level": "1",
+  "url": "sec-pizza-giordanos.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Giordano’s-style Deep Dish Pizza",
+  "body": " Giordano's-style Deep Dish Pizza  "
+},
+{
+  "id": "sec-pizza-pequods",
+  "level": "1",
+  "url": "sec-pizza-pequods.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Pequod’s-style Pizza",
+  "body": " Pequod's-style Pizza  "
+},
+{
+  "id": "sec-pizza-chicagotavern",
+  "level": "1",
+  "url": "sec-pizza-chicagotavern.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Chicago tavern-style pizza",
+  "body": " Chicago tavern-style pizza  Source: J. Kenji Lopez-Alt recipe, New York Times   Ingredients   Yield: 2 (14-inch) pizzas   For Each Pizza    Giardinera  3\/4 cup mild or hot    Semolina or cornmeal  For dusting    Pizza dough  One, stretched and cured    Pizza Sauce  1\/4 to 1 cup    Pecorino, Romano, or Parmesan  2 ounces, finely shredded    Mozzarella, full-fat low-moisture  8 to 12 ozes, shredded    Italian Sausage  8 ounces       Steps     At least 45 minutes before baking, set a baking steel or stone (larger than 14 inches, see Tips) on an oven rack in the lower-middle position and heat the oven to 500 degrees. Drain excess oil from the giardiniera in a fine-mesh strainer, tossing to get as much off as possible. Discard the oil and set aside the giardiniera.    When the oven is heated and you’re ready to bake, dust a pizza peel with semolina or cornmeal. Lift cured pizza skin with the parchment or butcher paper still attached. The top (exposed) side will be drier than the underside. Place the pizza skin on the peel with the drier side facing down (so the papered side faces up). Peel off the parchment paper or butcher paper and discard. Using a fork, poke the crust all over to prevent large bubbles from forming.    Spread the sauce evenly over the entire surface of the pizza, all the way to the edges. Sprinkle with half the Romano or Parmesan. Spread the mozzarella evenly over the entire surface, spreading it all the way to the edges. Use your fingertips to dollop marble-size pieces of the raw sausage all over the pizza. Sprinkle with the giardiniera.    Give the pizza peel a few shakes to make sure the pizza is still loose (if it sticks at all, using a metal spatula or pizza peel to loosen it), then transfer the pizza to the heated baking steel or stone. Bake until the pizza is as done as you like it. (If you like yours browned and bubbly in the center and lightly charred at the edges, that’ll be about 10 minutes, depending on your oven.) You can use a metal pizza peel to lift the edges of the pizza and peek underneath as it bakes to ensure the bottom is done to your liking.    Transfer the pizza to a cutting board and sprinkle immediately with the remaining Romano or Parmesan. Allow to rest for 3 minutes, cut into 1½- to 2-inch squares, and serve immediately. Let the oven reheat for at least 10 minutes before baking subsequent pizzas.      Tips:    This style of pizza works best with minimal toppings–two to three items max. Sausage and giardiniera is a classic combination in the Chicago area. I like using J.P. Graziano hot giardiniera, which is available by mail order.    Do not use Italian-style (fresh) mozzarella for this pizza. If you cannot find full-fat low-moisture mozzarella, use a combination of widely available part-skim low-moisture mozzarella and shredded Monterey Jack. The cheeses will melt better if you grate them yourself, but pre-shredded can be used in a pinch.    If you do not have a baking stone or steel at least 14 inches square, 12-inch pizzas can also be baked on smaller stones or on the back of a large overturned aluminum sheet tray set on an oven rack while the oven heats. Follow the recipe for 12-inch pizza dough balls as outlined in the Tip for the Chicago thin-crust (tavern-style) pizza dough.       Pizza Dough     Total Time  30 minutes, plus at least 3 hours and up to 5 days resting    Prep Time  5 minutes   Compared with many other styles of pizza dough, this one is relatively easy. It's stretched with a rolling pin, then allowed to sit uncovered overnight until it has lightly dried to the texture of a flour tortilla. This is the secret to getting the crunchiest, most cracker-crisp crust, and it also makes topping and moving the pizzas a snap. Give this recipe a try. If you do, there are a few things to note: Both mass measurements in grams and volume measurements in cups and spoons are provided. It's strongly advised to use the mass measurements for everything, but at the very least, use it for the flour. Because flour is compressible, a cup can vary in weight by as much as 50 percent, completely throwing off the hydration level of your dough. Also strongly recommended: Following the full three-to five-day cold fermentation in Step 3, which makes rolling easier and results in a lighter crust.   Ingredients   Yield: 2 (14-inch) crusts   Bread flour  300 grams (about 2.25 cups)    Sugar  7 grams (about 1.25 teaspoons)    Salt  7 grams (about 1.25 teaspoons table salt or 1.25 teaspoons kosher salt)    Instant yeast  1 gram (about 0.25 teaspoon)    Water, cold  150 grams (about 0.5 cup)    Canola oil  30 grams    Semolina or cornmeal  For dusting       Steps      Make the dough: Combine the flour. sugar. salt and yeast in the bowl of a stand mixer (see Tips). Whisk together to combine, then add the water and oil. Mix at low speed with the dough hook attachment. stopping occasionally to scrape down the sides of the bowl with a rubber spatula. until the dough comes together in a shaggy ball. Cover the bowl and let the dough rest for 10 minutes. Mix on medium-low speed until the dough is silky and pulls away from the sides of the bowl. about 5 minutes. Transfer the dough to a lightly floured work surface.     Divide the dough: Using a bench scraper or knife, cut the dough into two even pieces, about 250 grams each. Using lightly floured hands, shape each piece into a smooth ball, then lightly coat each with oil, using your hands to cover every surface. Transfer each ball to a quart-size zipper-lock bag or other sealable container at least three times its volume. (Quart-size plastic deli containers also work for this.)     Ferment the dough: For good results, let the dough rest at room temperature until doubled in volume, about 3 hours. For better results, refrigerate the dough overnight. For best results, refrigerate the dough for at least 3 and up to 5 days.     The day before baking, roll the dough: Transfer one dough ball to a work surface generously dusted with semolina or cornmeal. (Refrigerated dough can be rolled straight out of the fridge, though allowing it to rest covered for 1 hour at room temperature will make rolling easier.) Using a rolling pin, roll the dough into a very thin circle 14 inches in diameter, lifting and rotating it occasionally (use a ruler or measuring tape to ensure the right size). Repeat with the second ball. If the dough feels like it keeps bouncing back as you try and stretch it, cover it with an inverted bowl or a clean kitchen towel, let it rest for half an hour, then try rolling again.     Cure the dough: Transfer the doughs to a flat surface lined with parchment paper or butcher paper. Allow to rest uncovered at room temperature overnight. The top surface of the dough should end up dry to the touch with a leathery feel. If they puff at all during curing, you can push any bubbles with a fork and push them down to flatten them.      Tips: For this recipe, you'll want an accurate baking scale and enough room to leave your stretched dough out at room temperature for a night. Using a scale to measure your ingredients is strongly advised here - at the very least for the flour. Going without can throw off the hydration level of your dough, and by extension, the success of your dough.  If you want to make 2 (12-inch) pizzas instead, use 225 grams flour, 5 grams sugar, 5 grams salt, 1 gram yeast, 115 grams cold water and 22.5 grams oil. Use only 3\/4 of the toppings.  The dough can also be made in a food processor or by hand. To make with afood processor, combine all the ingredients and process until the dough forms a ball that rides around the blades. Continue running the food processor for 30 seconds, then transfer the dough to a lightly floured work surface and continue as directed in Step 2.  To make by hand, combine the dry ingredients in a bowl, add the oil and water, and stir with a wooden spoon until a stiff, shaggy dough is formed. Cover the bowl and let the dough rest for 10 minutes.Turn the dough out onto a lightly floured work surface and knead until smooth, 7 to 10 minutes. Proceed as directed in Step 2.    "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
